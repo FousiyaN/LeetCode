6da750ebfc55e00b07fb8142c2a8f1e41835020c
Fousiya N <fousiyafousy01@gmail.com>
2026-07-20 21:20:46 +0530

    Clean up comments in strStr function
    Removed comments and documentation from the strStr function.

--- strings/strings/0028_Find_the_Index_of_the_First_Occurrence_in_a_String.py
+++ strings/strings/0028_Find_the_Index_of_the_First_Occurrence_in_a_String.py
@@ -1,13 +1,3 @@
-"""
-LeetCode #28 - Find the Index of the First Occurrence in a String
-
-Approach:
-Used Python's built-in find() function.
-
-Time Complexity: O(n)
-Space Complexity: O(1)
-"""
-
 
 class Solution(object):
     def strStr(self, haystack, needle):
